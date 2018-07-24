2f9465baffd53161bd7465a2bb2fe917752ec47e
Anders Evenrud <andersevenrud@gmail.com>
2018-07-24 10:20:59 +0200

    Produce standalone build

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const mode = process.env.NODE_ENV || 'development';
+const minimize = mode === 'production';
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode,
+  devtool: 'source-map',
+  entry: [
+    path.resolve(__dirname, 'index.js'),
+  ],
+  optimization: {
+    minimize,
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      'icon.png'
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
