f599341c92c2eb4ad23f34f4f46987fe34a07d51
Nguyễn Anh Khoa <khoaakt@gmail.com>
2019-01-04 01:54:40 +0700

    Added Vietnamese (vi_VN) translations (#3)

--- locales.js
+++ locales.js
@@ -7,3 +7,8 @@ export const nb_NO = {
   LBL_SHOW_HIDDEN_FILES: 'Vis skjulte filer',
   LBL_MINIMALISTIC: 'Minimalistisk'
 };
+
+export const vi_VN = {
+  LBL_SHOW_HIDDEN_FILES: 'Hiện các tập tin ẩn',
+  LBL_MINIMALISTIC: 'Tối giản'
+};
