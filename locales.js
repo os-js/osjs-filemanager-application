7851a2502a52ee8dca36d3b338a87363a4d9a517
Matheus Felipe <matheusfelipeog@gmail.com>
2020-02-13 22:36:45 -0300

    Updated pt_BR translations

--- locales.js
+++ locales.js
@@ -19,5 +19,20 @@ export const vi_VN = {
 export const pt_BR = {
   LBL_SHOW_HIDDEN_FILES: 'Mostrar arquivos ocultos',
   LBL_MINIMALISTIC: 'Minimalista',
-  LBL_OPEN_WITH: 'Abrir com...'
+  LBL_OPEN_WITH: 'Abrir com...',
+  LBL_SHOW_DATE: 'Mostrar coluna Data',
+  LBL_STATUS: '{0} diretórios, {1} arquivos, {2} bytes no total',
+  LBL_DATE: 'Data', // FIXME: Move to client
+  LBL_LOADING: 'Carregando {0}',
+  DIALOG_MKDIR_MESSAGE: 'Criar novo diretório',
+  DIALOG_MKDIR_PLACEHOLDER: 'Novo diretório',
+  DIALOG_RENAME_MESSAGE: 'Renomear {0} ?',
+  DIALOG_DELETE_MESSAGE: 'Deletar {0} ?',
+  MSG_ERROR: 'Um erro ocorreu',
+  MSG_UPLOAD_ERROR: 'Falha ao fazer upload do(s) arquivo(s)',
+  MSG_READDIR_ERROR: 'Um erro ocorreu ao ler o diretório: {0}',
+  MSG_PASTE_ERROR: 'Falha ao colar arquivo(s)',
+  MSG_MKDIR_ERROR: 'Falha ao criar diretório',
+  MSG_RENAME_ERROR: 'Falha ao renomear',
+  MSG_DELETE_ERROR: 'Falha ao deletar'
 };
